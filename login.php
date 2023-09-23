f894fe829f217e923244196da28e85eafc9fb4e0
Pratik yadav <pratikyadav18604@gmail.com>
2023-09-23 14:53:12 +0530

    done

--- login.php
+++ login.php
@@ -268,7 +268,7 @@ function oninit() {
             xl:px-5 lg:flex-row">
                 <div class="w-full bg-cover relative max-w-md lg:max-w-2xl lg:w-7/12">
                     <div class="flex flex-col items-center justify-center w-full h-full relative lg:pr-10">
-                        <img class="object-cover" src="https://st2.depositphotos.com/4559651/6824/v/950/depositphotos_68246427-stock-illustration-minimal-city-map-for-gps.jpg" class="btn-" />
+                        <img class="object-cover" src="img/map_banner.png" class="btn-" />
                     </div>
                 </div>
                 <div class="w-full mt-20 mr-0 mb-0 ml-0 relative z-10 max-w-2xl lg:mt-0 lg:w-5/12">

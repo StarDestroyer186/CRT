0410028c303bcdf0dffdf52f1dbbcadde7244fde
Pratik yadav <pratikyadav18604@gmail.com>
2023-09-27 17:41:41 +0530

    done

--- mobile/mjs/mdevicelist.soc.js
+++ mobile/mjs/mdevicelist.soc.js
@@ -174,11 +174,11 @@ function addGroupItem(tid, selid, gid, gtxt, n, c, nc, si, x, y, i, d, t, ts, v,
     var tgkey = tid + "_" + gid;
     var $KG = Groups[tgkey];
     if(!$KG){
-        $tbody = $("<tbody></tbody>").appendTo("#" + tid);
-        $tr = $("<tr class='a' data-id='44'></tr>").attr("g", gid).attr("t", gtxt).appendTo($tbody);
+        $tbody = $("<div></div>").appendTo("#" + tid);
+        $tr = $("<div class='a'></div>").attr("g", gid).attr("t", gtxt).appendTo($tbody);
 		
         $KG = $tr;
-		$("<th style='display:flex; justify-items:between;'><span style='background-color: #fff ;'><input style='margin: 0px 4px; height: 16px; width: 16px;' type='checkbox' class='showall'></span>").attr("title",JS_SHOW_ALL).appendTo($tr).find("input").prop('checked', show).css({opacity: show ? 1.0 : 0.5});
+		$("<div style='display:flex; justify-items:between;'><span style='background-color: #fff ;'><input style='margin: 0px 4px; height: 16px; width: 16px;' type='checkbox' class='showall'></span>").attr("title",JS_SHOW_ALL).appendTo($tr).find("input").prop('checked', show).css({opacity: show ? 1.0 : 0.5});
 		$("<span style='background-color: #fff;'><input style='margin: 0px 4px; height: 16px; width: 16px;' type='checkbox' class='trackall'></span>").attr("title",JS_TRACK_ALL).appendTo($tr);
         $g = $("<span style='word-wrap:break-word;word-break:break-all;'></span></th>").text(gtxt).attr("colspan", "8");
         if(JS_DEFAULT_COLLAPSED == 0){

9d08e804afa1a9b696b7fea8f1c259d158ba784e
StarDestroyer186 <pratikyadav18604@gmail.com>
2023-09-30 13:30:05 +0530

    done

--- mobile/mjs/mdevicelist.soc.js
+++ mobile/mjs/mdevicelist.soc.js
@@ -498,8 +498,9 @@ function loadEventInfo(keyid, toastShow){
 				$("#event-table").animate({scrollTop:0},0);
 				for(var i=0; i<json.length; i++){
 					var jo = json[i];
-					var $tbody = $("<div class='event-table-body'></div>").appendTo($("#event-table"));
-					var $tr = $("<div style='display: flex;justify-content: space-between;'></div>").appendTo($tbody);
+					var $tbody = $("<div class='event-table-body' style='margin-top:5px'></div>").appendTo($("#event-table"));
+					var $tr = $("<div style='display: flex;justify-content: space-between;padding:5px 10px'></div>").appendTo($tbody);
+					var $icon = $("<div><svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' class='w-6 h-6'><path stroke-linecap='round' stroke-linejoin='round' d='M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0' /></svg></div>").appendTo($tr);
 					var $td_time = $("<div></div>").text($.format.date(jo.t, JS_DEFAULT_DATETIME_fmt_JS)).appendTo($tr);
 					var $td_object = $("<div></div>").text(jo.c).appendTo($tr);
 					var $td_event = $("<div></div>").text(jo.a).appendTo($tr);
